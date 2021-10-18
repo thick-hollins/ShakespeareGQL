05476a2d3686dc9766d7034b4a8e80f6396824ef
thick-hollins <alexnuttall@zoho.eu>
2021-10-18 16:39:04 +0100

    cors

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,8 @@ namespace gql
     {
     private readonly IConfiguration Configuration;
 
+    private readonly string AllowedOrigin = "allowedOrigin";
+
     public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -32,6 +34,12 @@ namespace gql
                 .AddType<CharacterType>()
                 .AddFiltering()
                 .AddSorting();
+
+            services.AddCors(option => {
+                option.AddPolicy("allowedOrigin",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +51,8 @@ namespace gql
             }
 
             app.UseRouting();
+
+            app.UseCors(AllowedOrigin);
 
             app.UseEndpoints(endpoints =>
             {
